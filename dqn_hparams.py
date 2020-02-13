3d0134c4ccb9a143e1d615764d5b64ed6b700a3e
bourcierj <julesbourcier1@gmail.com>
2020-02-13 18:49:35 +0100

    Hyperparameters

---
+++ dqn_hparams.py
@@ -0,0 +1,21 @@
+
+hparams_cartpole = dict(
+
+    gamma = 0.999,  #@TUNE
+    epsilon = 0.01,
+    epsilon_decay = 0.99999,
+    batch_size = 100,
+    replay_memory_capacity = 100000,
+    ctarget = 1000,
+    layers = [200],
+    lr = 0.001,
+    lr_decay = None,  # @TUNE
+    episode_count = 2400,
+    update_frequency = 1  # NOT USED
+    )
+
+
+hparams_lunarlander = dict(
+
+
+)
